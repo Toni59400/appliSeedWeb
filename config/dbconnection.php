d052b9e5271743797f0b2d6db07174991dedb6c2
Toni59400 <95470541+Toni59400@users.noreply.github.com>
2023-01-28 15:13:00 +0100

    mm

--- config/dbconnection.php
+++ config/dbconnection.php
@@ -2,6 +2,7 @@
     try{
         $db = new PDO('mysql:host='.$config['server'].'; dbname='.$config['dbname'], $config['login'], $config['password']); 
         $db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $bd->exec('SET NAMES utf8');
     } catch (Exception $e) {
         /*echo 'ECHEC :'.$e->getMessage();*/
         $db = NULL; 
