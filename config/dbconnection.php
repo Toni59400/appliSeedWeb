4b25c01ea62c40b91cefe6508ccff511a6b08fad
Toni59400 <95470541+Toni59400@users.noreply.github.com>
2023-01-29 16:48:00 +0100

    encodage de la connexion a la bdd en utf8

--- config/dbconnection.php
+++ config/dbconnection.php
@@ -1,6 +1,6 @@
 <?php
     try{
-        $db = new PDO('mysql:host='.$config['server'].'; dbname='.$config['dbname'], $config['login'], $config['password']); 
+        $db = new PDO('mysql:host='.$config['server'].'; dbname='.$config['dbname'].';charset=utf8', $config['login'], $config['password']); 
         $db -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (Exception $e) {
         /*echo 'ECHEC :'.$e->getMessage();*/
